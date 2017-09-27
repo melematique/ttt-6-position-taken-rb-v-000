84497e1507370c729c038cc31ad14e001a45794e
Melissa Sanchez <melissa90sanchez@gmail.com>
2017-09-27 20:00:41 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  index !==" "
+  index !== array[index_number]
 end
+
+  
